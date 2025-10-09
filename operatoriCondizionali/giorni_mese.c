197ca70643327b47d8d901f9fbd590320b1a907d
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-10-09 15:16:12 +0200

    Scrivere un programma che richieda all’utente mese (in formato numerico: 1 per gennaio, 12 per dicembre) e anno e stampi il numero di giorni di quel mese in quell’anno.

---
+++ operatoriCondizionali/giorni_mese.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int mese, anno;
+    printf("Inserisci un mese\n");
+    scanf("%d", &mese);
+    printf("Inserisci un anno\n");
+    scanf("%d", &anno);
+    if (mese == 1 || mese == 3 || mese == 5 || mese == 7 || mese == 8 || mese == 10 || mese == 12)
+    {
+        printf("il mese ha 31 giorni\n");
+    }
+    else if (mese == 2)
+    {
+        if (anno % 4 == 0 && anno % 100 != 0 || anno % 400 == 0)
+        {
+            printf("hai inserito un anno bisestile\n");
+            printf("il mese di febbraio ha 29 giorni\n");
+        }
+        else
+        {
+            printf("non hai inserito un anno bisestile\n");
+            printf("il mese di febbraio ha 28 giorni\n");
+        }
+    }
+    else
+    {
+        printf("il mese ha 30 giorni\n");
+    }
+}
