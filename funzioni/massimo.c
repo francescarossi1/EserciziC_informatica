0a0e00aad4324b2feae338b1105c393780beba99
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-10-16 14:22:09 +0200

    trova il valore massimo tra due numeri

---
+++ funzioni/massimo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int massimo(int a, int b)
+{
+    if (a >= b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int main()
+{
+    int a, b;
+    printf("Inserisci un primo valore intero \n");
+    scanf("%d", &a);
+    printf("Inserisci un secondovalore intero \n");
+    scanf("%d", &b);
+
+    printf("il massimo  è %d\n", massimo(a, b));
+
+    return 0;
+}
