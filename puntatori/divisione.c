657e9c40cde79c6ec82baea6add379cee15e9c1c
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-10-21 22:17:38 +0200

    divisione con puntatori

---
+++ puntatori/divisione.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void divisione(int dividendo, int divisore, int *pq, int *pr)
+{
+    *pq = dividendo / divisore;
+    *pr = dividendo % divisore;
+}
+
+int main()
+{
+    int a = 22, b = 7;
+    int q, r;
+    divisione(a, b, &q, &r);
+    printf("%d %d\n", q, r);
+    return 0;
+}
