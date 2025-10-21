12c4e1a6f4cf2ee88e83ae13161074b9532af65a
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-10-21 21:53:29 +0200

    es2.c assegnamento

---
+++ puntatori/assegnamento2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a = 1, b = 2;
+    int *pa, *pb;
+
+    pa = &a;
+    pb = &b;
+    pa = pb;
+    printf("%d %d\n", *pa, *pb);
+    *pa = 3;
+    printf("%d %d\n", *pa, *pb);
+}
