4c8da259a6f1689153a4d1ec3991deb86fde4080
Francesca Rossi <francesca.rossi@edu.unife.it>
2025-10-23 16:16:46 +0200

    ottimizzazione del codice

--- iterazioni/primo.c
+++ iterazioni/primo.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
-int main()
+int primo(int n)
 {
-    int n, conteggio_divisori = 0;
-    printf("Inserisci un numero \n");
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
+    int conteggio_divisori = 0;
+    for (int i = 2; i <= n - 1; i++)
     {
+        if (conteggio_divisori > 0)
+        {
+            return 0;
+        }
         if (n % i == 0)
         {
             conteggio_divisori++;
         }
     }
     if (conteggio_divisori == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n, conteggio_divisori = 0;
+    printf("Inserisci un numero \n");
+    scanf("%d", &n);
+
+    if (primo(n))
     {
         printf("%d è un numero primo \n", n);
     }
