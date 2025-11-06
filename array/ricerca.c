8112e9a4183e5c6e6f06cabbbbcc42cb3d558748
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-11-06 13:56:34 +0100

    ricerca elemento

---
+++ array/ricerca.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define DIM 5
+int main()
+{
+    int a[] = {4, 3, 8, 5, 1};
+    int trovato = 0, i, elemento;
+
+    printf("Inserisci un numero da cercare\n");
+    scanf("%d", &elemento);
+    i = 0;
+    while (!trovato && i < DIM)
+    {
+        if (a[i] == elemento)
+        {
+            trovato = 1;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    if (trovato)
+    {
+        printf("Trovato\n");
+    }
+}
