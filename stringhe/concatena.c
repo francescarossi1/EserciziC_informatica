098fba3c8ec70d2379e727198f0399d72fa98e7b
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-11-12 12:40:26 +0100

    concatenazione di stringhe

---
+++ stringhe/concatena.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void concatena(char *d, char *s1, char *s2)
+{
+
+    int i = 0, j = 0;
+
+    while (s1[i] != '\0')
+    {
+        d[i] = s1[i];
+        i++;
+    }
+
+    while (s2[j] != '\0')
+    {
+        d[i + j] = s2[j];
+        j++;
+    }
+
+    d[i + j] = '\0';
+}
+
+int main()
+{
+    char s1[] = "gian";
+    char s2[] = "luca";
+    char s3[40];
+
+    concatena(s3, s1, s2);
+
+    printf("%s\n", s3);
+}
