79a09ac673892cd98dfafc65e1dc54944971f074
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-11-14 07:39:37 +0100

    modifiche

--- numeri_casuali/dado.c
+++ numeri_casuali/dado.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
@@ -7,7 +8,7 @@ int main()
     srand(time(NULL));
     for (i = 0; i < 10; i++)
     {
-        printf("%d", 1 + rand() % 6);
+        printf("%d\n", 1 + rand() % 6);
     }
     printf("\n");
     return 0;
