2850fddb637bab6b8e087b413a42680fe0132a16
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-11-14 07:23:16 +0100

    modifiche

--- numeri_casuali/temperatura.c
+++ numeri_casuali/temperatura.c
@@ -3,10 +3,9 @@
 
 int main()
 {
-    int casuale;
     for (int i = 0; i < 10; i++)
     {
-        printf("%f \n", 15.0 + 10.0 * rand() / RAND_MAX);
+        printf("%.2f \n", 15.0 + 10.0 * rand() / RAND_MAX);
     }
     return 0;
 }
