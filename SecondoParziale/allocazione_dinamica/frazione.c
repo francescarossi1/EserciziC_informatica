8385600c813211b80672e4ffadff7751f91029d4
francescarossi1 <francescarossi1>
2025-12-06 17:54:40 +0100

    esercizi allocazione dinamica

---
+++ SecondoParziale/allocazione_dinamica/frazione.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int num;
+    int den;
+} Frazione;
+
+Frazione *frazione(int n, int d)
+{
+    Frazione *p;
+    p = (Frazione *)malloc(sizeof(Frazione));
+    p->num = n;
+    p->den = d;
+    return p;
+}
+
+int main()
+{
+    Frazione *pf;
+    pf = frazione(4, 3); // "new"
+}
