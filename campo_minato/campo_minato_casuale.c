85cb6788838fab6ba0fa4502410acd9fa48c77c9
Francesca Rossi <francesca16.rossi@edu.unife.it>
2025-12-07 20:31:25 +0100

    pt1

---
+++ campo_minato/campo_minato_casuale.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NRIGHE 6
+#define NCOLONNE 10
+
+float rnd_flt(float min, float max)
+{
+    return min + (float)rand() / RAND_MAX * (max - min); // genera numero casuale compreso da 0 a 1
+}
+
+void inizializza(char c[NRIGHE][NCOLONNE], float probmina)
+{
+    int i, j;
+    for (i = 0; i < NRIGHE; i++)
+    {
+        for (j = 0; j < NCOLONNE; j++)
+        {
+            if (rnd_flt(0.0, 1.0) <= probmina)
+            {
+                c[i][j] = '*';
+            }
+            else
+            {
+                c[i][j] = ' ';
+            }
+        }
+    }
+}
+
+void stampa(char c[NRIGHE][NCOLONNE])
+{
+    int i, j;
+    for (i = 0; i < NRIGHE; i++)
+    {
+        for (j = 0; j < NCOLONNE; j++)
+        {
+            printf("%c", c[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    char campo[NRIGHE][NCOLONNE];
+
+    inizializza(campo, 0.2);
+    stampa(campo);
+}
